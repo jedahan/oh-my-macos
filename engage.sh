1e01141176a1f20da42a69233a334c3b13189e00
Jonathan Dahan <jonathan@jonathan.is>
2015-10-03 14:53:13 -0400

    Show how to install brew to $HOME/.homebrew if its not installed

--- engage.sh
+++ engage.sh
@@ -1,11 +1,12 @@
-function msg {
-  echo "➜\033[1;32m $1 \033[0m";
+echo "hi"
+brew -v || {
+  echo "brew not found, please install with:"
+  echo 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install | sed -e \'s|usr/local|Users/jedahan/.homebrew|\')"'
+  exit 1
 }
 
-msg "hi"
-msg "root permissions are required for a few commands, asking upfront"
+echo "root permissions are required for a few commands, asking upfront"
 sudo -v
-
 # Keep-alive: update existing `sudo` time stamp until this script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
