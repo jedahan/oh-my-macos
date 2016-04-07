10278cf7245944efcdd44201eb11e2e056bded07
Jonathan Dahan <jdehan@etsy.com>
2016-04-07 11:04:04 -0400

    install emoji-cli

--- applications.sh
+++ applications.sh
@@ -24,6 +24,9 @@ sudo easy_install httpie
 sudo npm install --global keybase-installer
 keybase-installer
 
+# Install emoji-cli
+sudo npm install --global emoji-cli
+
 # Install some more cli utilities
 export GEM_HOME="${HOME}/.gems"
 export GEM_PATH="$GEM_HOME"
