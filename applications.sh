83edc3aa63a8e53f52356fa7d57a9b64bd36b0ba
Jonathan Dahan <jdehan@etsy.com>
2016-04-15 15:53:58 -0400

    applications: make more idempotent by checking for installed applications before installing them

--- applications.sh
+++ applications.sh
@@ -5,14 +5,97 @@ brew -v && {
   brew update
   brew upgrade
   brew install ruby
-  brew tap Homebrew/bundle
-  brew bundle
+
+  brew tap caskroom/cask
+  brew tap homebrew/games
+  brew tap neovim/neovim
+  brew tap caskroom/fonts
+  brew tap osx-cross/avr
+  brew tap caskroom/versions
+
+  function brewinstall() {
+    brew list $1 >/dev/null || brew install $_
+  }
+
+  function caskinstall() {
+    brew cask list $1 >/dev/null || brew cask install $_
+  }
+
+  brew list neovim >/dev/null || brew install --HEAD neovim
+
+  for important_cli in (git zsh python3 sift); do
+    brewinstall $important_cli
+  done
+
+  for important_app in (gpgtools firefox iterm2-beta font-inconsolata java slack tuxera-ntfs); do
+    caskinstall $important_app
+  done
+
+  for cli in (
+    aircrack-ng
+    android-sdk
+    avr-libc
+    avrdude
+    assh
+    boot2docker
+    fceux
+    gnuradio
+    go
+    hub
+    mtr
+    mpv
+    ngrep
+    nmap
+    node
+    ruby
+    trash
+    vice
+    watch
+    youtube-dl
+  ); do
+    brewinstall $cli
+  done
+
+  for app in (
+    alfred
+    android-file-transfer
+    arduino
+    atom
+    backblaze
+    battle-net
+    betterzipql
+    dash
+    dropbox
+    flickr-uploadr
+    fritzing
+    gitify
+    hex-fiend
+    horndis
+    mini-vmac
+    spotify
+    steam
+    transmission
+    vagrant
+    virtualbox
+    vlc
+    xquartz
+    qlcolorcode
+    qlmarkdown
+    qlprettypatch
+    qlstephen
+    quicklook-csv
+    quicklook-json
+    suspicious-package
+    zulip
+  ); do
+    caskinstall $app
+  done
+
   brew cleanup
   qlmanage -r
 }
 
-# Install commandline applications
-sudo easy_install httpie
+$+commands[http] || sudo easy_install httpie
 
 # Install keybase
 sudo npm install --global keybase-installer
