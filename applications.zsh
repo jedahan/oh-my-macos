1fde886362a037e21a33df15efdb51978b26cbbc
Jonathan Dahan <git@jonathan.is>
2024-06-30 14:46:33 -0400

    applications: inline all installs for simplicity

--- applications.zsh
+++ applications.zsh
@@ -4,84 +4,91 @@ set -e
 echo "Updating macOS software"
 sudo softwareupdate --install --all
 
+# ! brew leaves --installed-on-request
 brew -v && {
   echo "updating homebrew"
   brew update
-  brew upgrade --greedy
-  brew install ruby
-
-  important_clis=(
-    git
+  brew install \
+    avrdude \
+    aws-console \
+    awscli \
+    bat \
+    caddy \
+    cdrtools \
+    colima \
+    ddate \
+    dfu-util \
+    dive \
+    entr \
+    esbuild \
+    exiftool \
+    expect \
+    eza \
+    fd \
+    fdupes \
+    figlet \
+    fzf \
+    gallery-dl \
+    gammaray \
+    gh \
+    git \
+    git-lfs \
+    glow \
+    go \
+    helix \
+    jid \
+    lua-language-server \
+    lychee \
+    mas \
+    mtr \
+    nginx \
+    nmap \
+    node \
+    nycresistor/nycr/pdf2laser \
+    open-ocd \
+    oven-sh/bun/bun \
+    php \
+    podman \
+    postgresql@15 \
+    qt@5 \
+    ripgrep \
+    ruby \
+    rust \
+    rustup-init \
+    shellcheck \
+    socat \
+    squashfs \
+    superfile \
+    syncthing \
+    telnet \
+    tio \
+    tlrc \
+    tmux \
+    toilet \
+    tokei \
+    tree-sitter \
+    typescript-language-server \
+    vhs \
+    watch \
+    wireguard-tools \
+    xh \
+    zig \
+    zls \
+    zoxide \
     zsh
-    python3
-    ripgrep
-    tlrc
-    eza
-    helix
-    fd
-  )
-
-  important_apps=(
-    firefox
-    caskroom/fonts/font-inconsolata-nerd-font
-    zed
-  )
-
-  clis=(
-    avrdude
-    coreutils
-    hub
-    xh
-    jq
-    mpv
-    mtr
-    netcat6
-    ngrep
-    nmap
-    pngcheck
-    tcpdump
-    watch
-    youtube-dl
-  )
-
-  apps=(
-    arduino
-    atom
-    element
-    hex-fiend
-    mini-vmac
-    ngrok
-    retroarch
-    signal
-    slack
-    spotify
-    transmission
-    zulip
-  )
-
-  function brewinstall() { brew list $1 >/dev/null || brew install $_ }
-
-  function caskinstall() { brew cask list $1 >/dev/null || brew cask install $_ }
-
-  for cli in $important_clis; do brewinstall $cli; done
-  for app in $important_apps; do caskinstall $app; done
-  for cli in $clis; do brewinstall $cli; done
-  for app in $apps; do caskinstall $app; done
 }
 
+# cargo install --list | grep -v ':'
 (( $+commands[cargo] )) && {
-  crates=(
-    bat
-    cargo-edit
-    cargo-update
-    hyperfine
+  cargo install \
+    cargo-outdated \
+    elf2uf2-rs \
+    flip-link \
+    hyperfine \
+    libreddit \
+    probe-rs-cli \
+    probe-run \
+    cargo-tauri \
+    wireguard-vanity-address \
     zr
-  )
-
-  function crateinstall() {
-    cargo install --list | grep $1 >/dev/null || cargo install $_
-  }
-
-  for crate in $crates; do crateinstall $crate; done
 }
-
